fb87cf7d0f482d89f38894a27a65505e5d236ac1
mean <meenchutimon05@gmail.com>
2026-05-30 00:31:25 +0700

    test popup coupon

--- app/coupon/page.tsx
+++ app/coupon/page.tsx
@@ -321,7 +321,7 @@ export default function CouponPage() {
                   className="confirmBtn"
                   onClick={handleConfirmClaim} // สั่งบันทึกหักคะแนนจริง
                 >
-                  กดยืนยัน
+                  ยืนยัน
                 </button>
                 
                 <button
